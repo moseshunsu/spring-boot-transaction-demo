030e89325ffd065d5ccddbffcfe957701dfb85cc
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-03 19:17:10 +0100

    handled transaction using Spring @Transactional Annotation

--- src/main/java/com/higherAchievers/springboottransactiondemo/service/impl/OrderServiceImpl.java
+++ src/main/java/com/higherAchievers/springboottransactiondemo/service/impl/OrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.higherAchievers.springboottransactiondemo.repository.OrderRepository;
 import com.higherAchievers.springboottransactiondemo.repository.PaymentRepository;
 import com.higherAchievers.springboottransactiondemo.service.OrderService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -24,6 +25,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public OrderResponse placeOrder(OrderRequest orderRequest) {
 
         Order order = orderRequest.getOrder();
